a75462280315dbe5ac58dd8e87a50367388315b1
Rheanna Howell <78620534+Howellr9948@users.noreply.github.com>
2021-02-19 23:07:11 -0500

    Add files via upload
    Program 14 on page 114 otherwise known as M4CW1_Howell

---
+++ FieldTest.java
@@ -0,0 +1,30 @@
+/*
+ * Rheanna Howell
+ * CIS-115-0901
+ * 20210219
+ * Program 14 on page 114
+ */
+package m4cw1_howell;
+
+public class FieldTest 
+{
+    public static final int VLAUE = 10;
+    
+    public static void main(String[] args)
+    {
+        //Statments here have access to
+        //the VALUE constant.
+    }    
+    
+    public static void method2()
+    {
+        //Statments here have access to
+        // the VALUE constant.
+    }
+    
+    public static void method3()
+    {
+        //Statments here have access to
+        //the VALUE constant.
+    }
+}
